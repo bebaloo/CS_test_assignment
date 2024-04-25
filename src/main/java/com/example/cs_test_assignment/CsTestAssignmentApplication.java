4996dd9bc2aa85e7cdad951c3ba8afc36bbcc8f9
Dmytro Tkachuk <dmytrooo3.tk@gmail.com>
2024-04-26 00:36:08 +0300

    Init commit

---
+++ src/main/java/com/example/cs_test_assignment/CsTestAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.cs_test_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsTestAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsTestAssignmentApplication.class, args);
+	}
+
+}
