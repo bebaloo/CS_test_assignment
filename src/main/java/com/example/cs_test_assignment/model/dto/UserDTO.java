428f5c07214fd5ecece3367c9058e21520e6b4d3
Dmytro Tkachuk <dmytrooo3.tk@gmail.com>
2024-04-26 03:57:59 +0300

    feat: add age validation

--- src/main/java/com/example/cs_test_assignment/model/dto/UserDTO.java
+++ src/main/java/com/example/cs_test_assignment/model/dto/UserDTO.java
@@ -1,11 +1,19 @@
 package com.example.cs_test_assignment.model.dto;
 
+import com.example.cs_test_assignment.validation.annotation.Adult;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Past;
+
 import java.time.LocalDate;
 
-public record UserDTO(String email,
-                      String firstName,
-                      String lastname,
-                      LocalDate birthDate,
-                      String address,
-                      String phoneNumber) {
+public record UserDTO(
+        @Email
+        String email,
+        String firstName,
+        String lastname,
+        @Past
+        @Adult
+        LocalDate birthDate,
+        String address,
+        String phoneNumber) {
 }
